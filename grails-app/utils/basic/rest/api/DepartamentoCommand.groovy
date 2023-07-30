d830fcb9dbcc84c2d9820b1b871aea7cf62d33a3
Leonardo Melzer <leoimelzer@gmail.com>
2023-07-30 10:53:31 -0300

    fix: implements Validateable to Command classes

--- grails-app/utils/basic/rest/api/DepartamentoCommand.groovy
+++ grails-app/utils/basic/rest/api/DepartamentoCommand.groovy
@@ -1,6 +1,13 @@
 package basic.rest.api
 
-class DepartamentoCommand {
+import grails.validation.Validateable
+
+class DepartamentoCommand implements Validateable {
     Long id
     String nome
+
+    static constraints = {
+        id generator: 'increment', primary: true
+        nome nullable: false, blank: false, unique: true, maxSize: 120
+    }
 }
--- grails-app/utils/basic/rest/api/EmpregadoCommand.groovy
+++ grails-app/utils/basic/rest/api/EmpregadoCommand.groovy
@@ -1,11 +1,19 @@
 package basic.rest.api
 
+import grails.validation.Validateable
+
 import java.time.LocalDate
 
-class EmpregadoCommand {
+class EmpregadoCommand implements Validateable {
     Long id
     String nome
     LocalDate dataNascimento
     Integer matricula
     Long departamentoId
+
+    static constraints = {
+        id generator: 'increment', primary: true
+        nome nullable: false, blank: false, maxSize: 120
+        matricula nullable: false, unique: 'departamento'
+    }
 }
