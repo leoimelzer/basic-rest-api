cffa6b790a27062bdf863256c6bc89bd9e482e8e
Leonardo Melzer <leoimelzer@gmail.com>
2023-07-30 11:00:08 -0300

    chore: alter indentation

--- grails-app/init/basic/rest/api/BootStrap.groovy
+++ grails-app/init/basic/rest/api/BootStrap.groovy
@@ -8,37 +8,44 @@ class BootStrap {
 
     def init = { servletContext ->
         println "Initializing data..."
+
         departamentoService.save(new DepartamentoCommand(nome: "Insoft4 Sistemas de Informação"))
         departamentoService.save(new DepartamentoCommand(nome: "Insoft4 Equipamentos"))
         empregadoService.save(new EmpregadoCommand(
             nome: "Keith",
             dataNascimento: LocalDate.of(2000, 7, 29),
             matricula: 1,
-            departamentoId: 2)
+            departamentoId: 2
+            )
         )
         empregadoService.save(new EmpregadoCommand(
             nome: "Alice",
             dataNascimento: LocalDate.of(1998, 8, 16),
             matricula: 1,
-            departamentoId: 1)
+            departamentoId:
+                        1
+            )
         )
         empregadoService.save(new EmpregadoCommand(
             nome: "Robin",
             dataNascimento: LocalDate.of(1992, 7, 20),
             matricula: 2,
-            departamentoId: 1)
+            departamentoId: 1
+            )
         )
         empregadoService.save(new EmpregadoCommand(
             nome: "Woody",
             dataNascimento: LocalDate.of(1995, 11, 15),
             matricula: 2,
-            departamentoId: 2)
+            departamentoId: 2
+            )
         )
         empregadoService.save(new EmpregadoCommand(
             nome: "Guthrie",
             dataNascimento: LocalDate.of(2002, 03, 23),
             matricula: 3,
-            departamentoId: 1)
+            departamentoId: 1
+            )
         )
     }
     def destroy = {
